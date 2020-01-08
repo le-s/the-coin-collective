67e9c2a48208fb2d4fac5cf22a8714e001b41e8a
Steven Le <steven.an.le@gmail.com>
2020-01-07 18:38:19 -0800

    Create project skeleton

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoinIndex from './coin_index.jsx'
+
+const App = () => {
+  return (
+    <div className="App">
+      <CoinIndex />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/coin_index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class CoinIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }  
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {/* {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))} */}
+        </ul>
+      );
+    }
+  }
+}
+
+export default CoinIndex
