fac10bae5cdbc8632c924f345e75f59c0ee38c0b
Steven Le <steven.an.le@gmail.com>
2020-01-14 21:54:52 -0800

    Updates

---
+++ precache-manifest.6864d1e85d429e339f9af5aa098fb9d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b3d81e662c4e657caf73e3af4356d2",
+    "url": "/the-coin-collective/index.html"
+  },
+  {
+    "revision": "c1d6f4efbd1118acbc59",
+    "url": "/the-coin-collective/static/css/main.4aae4f85.chunk.css"
+  },
+  {
+    "revision": "cc903b4c5c68a30eb68a",
+    "url": "/the-coin-collective/static/js/2.4e0e2566.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/the-coin-collective/static/js/2.4e0e2566.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1d6f4efbd1118acbc59",
+    "url": "/the-coin-collective/static/js/main.069cb852.chunk.js"
+  },
+  {
+    "revision": "879cda6284e6a1c2105a",
+    "url": "/the-coin-collective/static/js/runtime-main.3d11dbad.js"
+  },
+  {
+    "revision": "2eee19992f60a61e37520210e7c27386",
+    "url": "/the-coin-collective/static/media/portfolio.2eee1999.ico"
+  }
+]);
